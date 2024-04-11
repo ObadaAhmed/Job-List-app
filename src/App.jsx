71d1701f87e5741b91f93bc52ed179a46b0f5b25
Obada-Ahmed <144023346+Obada-Ahmed@users.noreply.github.com>
2024-04-11 10:46:10 +0400

    App basic setup and layout

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {Route , createBrowserRouter , createRoutesFromElements , RouterProvider} from 'react-router-dom'
+import React from 'react'
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobPage from './pages/JobPage';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobPage />} />
+      <Route path='*' element={<NotFound />} />
+    </Route> 
+)
+)
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
+
+export default App
